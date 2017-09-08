50b41f3750d6a394ff0d31441adccc6ed1a106d8
OrrinBlake <orrinblake@gmail.com>
2017-09-08 19:42:06 +0100

    Initial Commit

---
+++ Swift App 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 1
+//
+//  Created by orrin blake on 08/09/2017.
+//  Copyright © 2017 SwiftApp1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
